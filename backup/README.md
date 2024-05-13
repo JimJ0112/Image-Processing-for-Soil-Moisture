599ec548bbf091518cbd3f5757ef24004c5b3adc
JimJ0112 <68044290+JimJ0112@users.noreply.github.com>
2024-05-13 22:32:04 +0800

    removed manual input

--- backup/README.md
+++
@@ -1,4 +0,0 @@
-# Image-Processing-for-Soil-Moisture
-# uses imagecolorat() to get rgb values of pixel 
-# run using xampp -> apache server
-# localhost://foldername/image_processing.php to see results
--- machine_learning/ml_sample.php
+++ machine_learning/ml_sample.php
@@ -21,6 +21,17 @@ $data_set->getTestSamples();
 $data_set->getTestLabels();
 
 
+$trainSamples = $data_set->getTestSamples();
+/*
+echo "<table> <thead> <tr> <th>DN(Red)</th> <th> DN(Green) </th> <th> DN(Blue) </th></tr> </thead> <tbody> ";
+foreach ($trainSamples as $trainsample) {
+    echo "<tr> <td>" . $trainsample[0] . "</td> <td> " . $trainsample[1] . "</td> <td>" . $trainsample[2] . "</td> </tr> ";
+}
+echo "</tbody> </table>";
+*/
+$data_set->getTrainLabels();
+$data_set->getTestSamples();
+print_r($data_set->getTestLabels());
 // training a model
 //$regression = new SVR(Kernel::LINEAR);
 $regression = new LeastSquares();
@@ -38,13 +49,18 @@ $regression = $modelManager->restoreFromFile("models/regressionModel_rgb_least_s
 */
 
 $predict = $regression->predict($data_set->getTestSamples());
+echo "<table> <tbody> ";
+foreach ($predict as $pred) {
+    echo "<tr> <td>" . $pred . "</td> </tr>";
+}
+echo "</table> </tbody> ";
 
 $score = Regression::r2Score($data_set->getTestLabels(), $predict);
 $accuracy = Accuracy::score($data_set->getTestLabels(), $predict);
 echo "<br/>R2 Score is: " . $score . "<br/>";
 echo "<br/> <br/>";
 
-
+/*
 $testArray = array(array(47.568518518518516, 40.21111111111111, 33.916666666666664), array(58.24074074074074, 49.75370370370371, 40.28703703703704), array(69.39814814814815, 60.757407407407406, 52.45), array(72.41481481481482, 60.44814814814815, 49.32592592592592), array(64.85185185185185, 57.08148148148148, 48.54074074074074), array(60.483333333333334, 48.05740740740741, 37.43518518518518));
 $count = 25;
 foreach ($testArray as $data) {
@@ -52,4 +68,4 @@ foreach ($testArray as $data) {
     echo $regression->predict($data);
     echo "<br/> <br/>";
     $count++;
-}
+}*/
